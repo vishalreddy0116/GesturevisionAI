bf00769879f5c632895d1778bd1a4c955399fdbd
vishal <vishalreddy0116@gmail.com>
2026-08-04 16:00:59 +0530

    feat: initialize MediaPipe hand tracker

--- app/vision/hand_tracker.py
+++ app/vision/hand_tracker.py
@@ -4,4 +4,13 @@ import mediapipe as mp
 class HandTracker:
 
     def __init__(self):
-        pass
+        self.mp_hands = mp.solutions.hands
+        self.mp_draw = mp.solutions.drawing_utils
+
+        self.hands = self.mp_hands.Hands(
+            static_image_mode=False,
+            max_num_hands=1,
+            model_complexity=1,
+            min_detection_confidence=0.7,
+            min_tracking_confidence=0.7,
+        )
